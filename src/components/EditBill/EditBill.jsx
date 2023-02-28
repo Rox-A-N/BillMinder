12cba3c52a6ae0c24f7d71b7bba8a11128a6806a
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2023-02-28 15:25:27 -0600

    EditBill component with tested route to UserPage

--- src/components/EditBill/EditBill.jsx
+++ src/components/EditBill/EditBill.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import {useSelector} from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
@@ -8,11 +10,24 @@ function EditBill(props) {
   // Using hooks we're creating local state for a "heading" variable with
   // a default value of 'Functional Component'
   const store = useSelector((store) => store);
+  const history = useHistory();
+
   const [heading, setHeading] = useState('Edit Bill Component');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('in Edit Bill Submit', props);
+    submitBill();
+  }
+
+  const submitBill = () => {
+    history.push('/user');
+  }
 
   return (
     <div>
       <h2>{heading}</h2>
+      <button className="Submit" onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
