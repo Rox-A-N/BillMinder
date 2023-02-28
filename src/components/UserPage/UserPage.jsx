7738f175dc374d3303334ffc443e258d6eb770c4
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2023-02-28 15:24:31 -0600

    I made & tested a route to the EditBill component

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
+  const history = useHistory();
   const user = useSelector((store) => store.user);
+
+  const handleEdit = (event) => {
+    event.preventDefault();
+    console.log('the edit button has been clicked by user', user);
+    routeToEditBill();
+  }
+
+  const routeToEditBill = () => {
+    history.push('/edit');
+  }
+
   return (
-    <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
-    </div>
+    <>
+      <button className="edit" onClick={handleEdit}>Add Bill</button>
+      <div className="container">
+        <h2>Welcome, {user.username}!</h2>
+        <p>Your ID is: {user.id}</p>
+        <LogOutButton className="btn" />
+      </div>
+    </>
   );
 }
 
