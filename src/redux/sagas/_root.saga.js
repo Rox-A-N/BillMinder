c273f3afa5a8a3b536215333414e0ba64050b98c
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2023-03-01 21:46:45 -0600

    added billSaga to fetch db requests

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -2,6 +2,7 @@ import { all } from 'redux-saga/effects';
 import loginSaga from './login.saga';
 import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
+import billSaga from './bill.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -10,10 +11,12 @@ import userSaga from './user.saga';
 // some sagas trigger other sagas, as an example
 // the registration triggers a login
 // and login triggers setting the user
+// billSaga triggers the fetchBills generator function getting info from the db
 export default function* rootSaga() {
   yield all([
     loginSaga(), // login saga is now registered
     registrationSaga(),
     userSaga(),
+    billSaga(),
   ]);
 }
