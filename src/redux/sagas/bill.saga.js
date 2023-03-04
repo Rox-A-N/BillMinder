805c87868391d2db928b914a2704780dc871ee60
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2023-03-04 12:44:37 -0600

    code cleanup

--- src/redux/sagas/bill.saga.js
+++ src/redux/sagas/bill.saga.js
@@ -12,16 +12,10 @@ function* fetchBills() {
       withCredentials: true,
     };
 
-    // the config includes credentials which
-    // allow the server session to recognize the user
-    // If a user is logged in, this will return their information
-    // from the server session (req.user)
     const response = yield axios.get('/api/bills', config);
     console.log('RESPONSE', response.data);
 
-    // now that the session has given us a user object
-    // with an id and username set the client-side user object to let
-    // the client-side code know the user is logged in
+ 
     yield put({ type: 'GET_BILLS', payload: response.data });
   } catch (error) {
     console.log('User get request failed', error);
@@ -33,3 +27,5 @@ function* billSaga() {
 }
 
 export default billSaga;
+
+
