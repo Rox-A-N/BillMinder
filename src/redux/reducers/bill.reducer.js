4eb593083982d1d7a3a97fd2c4adabc1e7eb0b25
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2023-03-07 19:27:44 -0600

    component comments, switch-case to add post action

--- src/redux/reducers/bill.reducer.js
+++ src/redux/reducers/bill.reducer.js
@@ -1,7 +1,12 @@
+// Defining 'bill' in the Redux store to be accessed by the BillPage component
+// action parameter of billReducer has two properties: payload & type
+
 const billReducer = (state = [], action) => {
     console.log('in billReducer', action.payload);
     switch (action.type) {
       case 'GET_BILLS':
+        return action.payload;
+      case 'POST_BILLS':
         return action.payload;
       default:
         return state;
@@ -11,4 +16,24 @@ const billReducer = (state = [], action) => {
   // user will be on the redux state at:
   // state.bills
   export default billReducer;
+
+
+  // saw this solution: 
+// switch (action.type) {
+// case 'FETCH BILL':
+// -- HANDLE THE FETCH_BILLS action--
+//  return state; // return the updated state
+//
+//  case 'ADD_BILL':
+// --HANDLE THE ADD_BILL action--
+//  return [...state, action.payload]; --return the updated state with the new bill
+//
+//  case 'DELETE_BILL':
+//  --HANDLE THE DELETE_BILL action--
+//  return state.filter(bill => bill.id !== action.payload.id); --return the updated state without the deleted bill
+//
+//  default:
+//      return state;
+// }
+
   
