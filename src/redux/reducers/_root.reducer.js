22a3aa57ba62b53709a31010799c1e29d6dcba62
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2023-03-07 19:28:11 -0600

    corrected reducer name from billReducer

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
-import bills from './bill.reducer';
+import billReducer from './bill.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -12,7 +12,7 @@ import bills from './bill.reducer';
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
-  bills, // will pull up the bills from the db
+  billReducer, // will pull up the bills from the db
 });
 
 export default rootReducer;
