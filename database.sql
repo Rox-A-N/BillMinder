bc3598e36a1c9a605564d918be9cb732d0cf43bb
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2023-03-07 19:34:02 -0600

    Update to bill_data table in sql file

--- database.sql
+++ database.sql
@@ -8,3 +8,18 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+
+-- Table to create bill form entries
+CREATE TABLE "bill_data" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR (80) UNIQUE NOT NULL,
+	"amount" INTEGER,
+	"due date" DATE,
+	"category" VARCHAR (80),
+	"repeat" VARCHAR (100),
+	"payment method" VARCHAR (255),
+	"payment status" VARCHAR (255),
+	"cleared bank" BOOLEAN,
+	"notes" VARCHAR (255),
+	"user_id" INT REFERENCES "user");
