a808be605afc2ee9b93ce1aff4820437b034ddd9
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2023-03-07 19:30:55 -0600

    added post route to send data to db

--- server/routes/bills.router.js
+++ server/routes/bills.router.js
@@ -19,11 +19,25 @@ router.get('/', (req, res) => {
   })
 });
 
-/**
- * POST route template
- */
+// /**
+//  * POST route template
+//  */
+
 router.post('/', (req, res) => {
   // POST route code here
+  console.log('inside POST route', req.body);
+  let queryText = `
+  INSERT INTO "bill_data" ("user_id", "name", "amount", "due_date")
+  VALUES ($1, $2, $3, $4)
+  RETURNING "id";
+  `;
+  pool.query(queryText, [req.user.id, req.body.newBill.name, req.body.newBill.amount, req.body.newBill.due_date] ).then((result) => {
+    console.log('Response in POST:', req.body);
+    res.sendStatus(200);
+  }).catch(error => {
+    console.log(error);
+    res.sendStatus(500);
+  })
 });
 
 module.exports = router;
